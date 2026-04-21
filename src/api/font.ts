ac93248c71a7984dd1d9303ea1d9b166dda490aa
SilentDepth <github@silent.land>
2026-04-21 14:20:49 +0800

    🐛 font loading issue

--- src/api/font.ts
+++ src/api/font.ts
@@ -1,14 +1,9 @@
-import { readFile } from 'node:fs/promises'
-import { resolve } from 'node:path'
-import { fileURLToPath } from 'node:url'
-import { createIsomorphicFn } from '@tanstack/react-start'
 import { joinURL } from 'ufo'
 
-const loadFontFile = createIsomorphicFn()
-  .server((name: string) =>
-    readFile(resolve(fileURLToPath(import.meta.url), '../../../public/assets/fonts', name)),
+const loadFontFile = (name: string) =>
+  fetch(joinURL(`${process.env.SITE_URL || process.env.VERCEL_URL || ''}/assets/fonts`, name)).then(
+    res => res.arrayBuffer(),
   )
-  .client((name: string) => fetch(joinURL('/assets/fonts', name)).then(res => res.arrayBuffer()))
 
 export async function loadFonts() {
   const [NotoSerif] = await Promise.all([
